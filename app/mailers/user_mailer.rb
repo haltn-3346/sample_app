06f86a0c5a1b8b5753b90ab6ef00ec3aa5461397
ngocha198 <luu.thi.ngoc.ha@sun-asterisk.com>
2022-08-04 17:21:14 +0700

    chapter 12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,9 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: t("mailers.account_activation_sub")
   end
+
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("mailers.password_reset")
+  end
 end
