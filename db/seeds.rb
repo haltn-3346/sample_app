b7f82359a118559dccb4648c158ce43e24ee1a1a
ngocha198 <luu.thi.ngoc.ha@sun-asterisk.com>
2022-08-04 12:08:57 +0700

    Chapter 11

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,9 @@ User.create!(name: "ngocha",
              email: "yi28112000@gmail.com",
              password: "12345678",
              password_confirmation: "12345678",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
@@ -13,5 +15,7 @@ User.create!(name: "ngocha",
   User.create!(name: name,
                email: email,
                password: password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
   end
